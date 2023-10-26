d60347dcec821e3d7e6b4469562c923924f338de
Balram Rajput <56222209+Balram-Rajput@users.noreply.github.com>
2023-10-26 14:08:52 +0530

    Production API - update

---
+++ src/app/globals.ts
@@ -0,0 +1,8 @@
+export class Globals{
+    
+    public static get Prod_BaseURL():string {return "https://city-gis-server.onrender.com/api/v1" }
+
+    public static get Test_BaseURL():string {return "http://localhost:3000/api/v1" }
+}
+
+

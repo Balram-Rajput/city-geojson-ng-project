4c3fab2de242d4a962a4b360240554e0e80f55e7
Balram Rajput <56222209+Balram-Rajput@users.noreply.github.com>
2023-10-26 18:47:01 +0530

    ng update

--- src/app/get-geojson/geojson.service.ts
+++ src/app/get-geojson/geojson.service.ts
@@ -10,8 +10,8 @@ export class GeojsonService {
 
   constructor( private http:HttpClient ) { }
 
-  URL = Globals.Prod_BaseURL
-  // URL = Globals.Test_BaseURL
+  // URL = Globals.Prod_BaseURL
+  URL = Globals.Test_BaseURL
   GetLocationData(search_data){
 
     return this.http.get<any>(`https://nominatim.openstreetmap.org/search?q=${search_data}&format=geojson`)
